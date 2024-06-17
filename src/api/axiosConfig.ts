bda50313a328f3eb66e356e1cd35a39173593ca4
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-06-17 18:39:38 +0330

    create folder structure

---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+class ContentApi {
+    private static _instance = axios.create({
+        baseURL: "https://6606fe26be53febb857efe71.mockapi.io/rent-house/v1/",
+        timeout: 8000,
+        headers: {
+            Accept: "application/json",
+        },
+    });
+
+    static get instance () {
+        return ContentApi._instance;
+    }
+}
+
+class AccountApi {
+    private static _instance = axios.create({
+        baseURL: "https://api.escuelajs.co/api/v1/",
+        timeout: 8000,
+        headers: {
+            Accept: "application/json",
+        },
+    });
+
+    static get instance () {
+        return AccountApi._instance;
+    }
+}
+
+export {ContentApi , AccountApi};
