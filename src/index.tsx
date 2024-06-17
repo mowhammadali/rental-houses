40a29cc6284f432fa7820a95fe759557be483a26
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-06-17 20:53:50 +0330

    initial redux config

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './store/store';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
