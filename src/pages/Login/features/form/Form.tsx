9e9254c2891564608ac3c86fb1f07ffade219630
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-07-13 22:28:05 +0330

    add dark mode styles to login and register

--- src/pages/Login/features/form/Form.tsx
+++ src/pages/Login/features/form/Form.tsx
@@ -5,7 +5,7 @@ import PulseLoader from "react-spinners/PulseLoader";
 import Notification from '../../../../components/common/notification/Notification';
 import { AxiosError } from 'axios';
 import { setCookie } from 'react-use-cookie';
-import { useState } from 'react';
+import { useState , useContext } from 'react';
 import { NavLink , useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -13,6 +13,7 @@ import { FaRegEye } from "react-icons/fa6";
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { RegisterInputstype } from '../../../../types/commonTypes';
 import { loginUser } from '../../../../api/requests';
+import { ThemeContext } from '../../../Layout/Layout';
 
 const schema = yup.object().shape({
     email: yup.string().required('لطفا ایمیل خود را وارد کنید').email('ایمیل نامعتبر است'),
@@ -20,6 +21,9 @@ const schema = yup.object().shape({
 });
 
 const Form = () => {
+    const themeParams = useContext(ThemeContext);
+    const isDark: boolean | undefined = themeParams?.isDark;
+
     const { register , handleSubmit , formState: {errors} , reset } = useForm<Omit<RegisterInputstype , 'avatar' | 'name'>>({
         resolver: yupResolver(schema),
     });
@@ -73,7 +77,7 @@ const Form = () => {
         <form className={css.formContainer} onSubmit={handleSubmit(onSubmit)}>
             <div className={css.header}>
                 <h1 className={css.title}>ورود</h1>
-                <img src="./assets/icons/main/dark-villasan-64.png" className={css.villasanIcon} alt="villasan icon"
+                <img src={isDark ? "./assets/icons/main/light-villasan-64.png" : "./assets/icons/main/dark-villasan-64.png"} className={css.villasanIcon} alt="villasan icon"
                     onClick={() => navigate('/')}/>
             </div>
             <div className={css.fields}>
