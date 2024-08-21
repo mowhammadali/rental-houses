2d8eacbedcc7a1bac06a2fbe39a59ea073aabc17
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-08-21 23:06:19 +0330

    change skeleton theme according to app's theme

--- src/pages/Home/components/carousel/Carousel.tsx
+++ src/pages/Home/components/carousel/Carousel.tsx
@@ -1,9 +1,11 @@
 import css from "./Carousel.module.css";
 import CarouselCard from "../carouselCard/CarouselCard";
-import Skeleton from 'react-loading-skeleton'
+import Skeleton , { SkeletonTheme } from 'react-loading-skeleton';
+import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { HouseType } from "../../../../types/commonTypes";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { ThemeContext } from "../../../Layout/Layout";
 
 type PropsType = {
     houses: HouseType[];
@@ -17,6 +19,8 @@ const Carousel = ({houses, isLoading, isFetching, title, state}: PropsType): JSX
     const [respectiveHouses, setRespectiveHouses] = useState<[] | HouseType[]>([]);
     const isPending = isLoading || isFetching;
 
+    const themeDetails = useContext(ThemeContext);
+
     useEffect(() => {
         if (!isLoading) {
             const newHouses = houses.filter((house) => house.state === state);
@@ -26,40 +30,42 @@ const Carousel = ({houses, isLoading, isFetching, title, state}: PropsType): JSX
 
     return (
         <div className={css.carouselContainer}>
-            <div className={css.info}>
-                {
-                    isPending
-                    ?
-                    <Skeleton style={{width: '150px' , height: '24px'}} borderRadius={8}/>
-                    :
-                    <p className={css.title}>{title}</p>
-                }
-                {
-                    isPending
-                    ?
-                    <Skeleton style={{width: '110px' , height: '40px'}} borderRadius={8}/>
-                    :
-                    <button className={css.showAllButton}>مشاهده همه</button>
-                }
-            </div>
-            <>
-                <Swiper
-                    slidesPerView={"auto"}
-                    spaceBetween={20}
-                    loop={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[]}
-                    className={css.mySwiper}
-                >
-                    {respectiveHouses.map((house) => (
-                        <SwiperSlide key={house.id} className={css.swiperSlide}>
-                            <CarouselCard house={house} isLoading={isLoading} isFetching={isFetching}/>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </>
+            <SkeletonTheme baseColor={themeDetails?.isDark ? '#1e293b' : '#e2e8f0'} highlightColor={themeDetails?.isDark ? "#334155" : '#cbd5e1'}>
+                <div className={css.info}>
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '150px' , height: '24px'}} borderRadius={8}/>
+                        :
+                        <p className={css.title}>{title}</p>
+                    }
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '110px' , height: '40px'}} borderRadius={8}/>
+                        :
+                        <button className={css.showAllButton}>مشاهده همه</button>
+                    }
+                </div>
+                <>
+                    <Swiper
+                        slidesPerView={"auto"}
+                        spaceBetween={20}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[]}
+                        className={css.mySwiper}
+                    >
+                        {respectiveHouses.map((house) => (
+                            <SwiperSlide key={house.id} className={css.swiperSlide}>
+                                <CarouselCard house={house} isLoading={isLoading} isFetching={isFetching}/>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </>
+            </SkeletonTheme>
         </div>
     );
 };
--- src/pages/Home/components/carouselCard/CarouselCard.tsx
+++ src/pages/Home/components/carouselCard/CarouselCard.tsx
@@ -1,6 +1,7 @@
 import css from "./CarouselCard.module.css";
-import Skeleton from 'react-loading-skeleton'
+import Skeleton , {SkeletonTheme } from 'react-loading-skeleton'
 import classNames from "classnames";
+import { useContext } from "react";
 import { useState } from "react";
 import { HouseType } from "../../../../types/commonTypes";
 import { FaStar } from "react-icons/fa";
@@ -10,6 +11,7 @@ import { englishToPersian } from "replace-persian-number";
 import { calculateDiscount } from "../../../../utils/calculateDiscount/calculateDiscount";
 import { GoHeart } from "react-icons/go";
 import { GoHeartFill } from "react-icons/go";
+import { ThemeContext } from "../../../Layout/Layout";
 
 type PropsType = {
     house: HouseType | undefined;
@@ -21,92 +23,96 @@ const CarouselCard = ({ house , isLoading , isFetching }: PropsType): JSX.Elemen
     const [isBeloved, setIsBeloved] = useState<boolean>(false);
     const isPending = isLoading || isFetching;
 
+    const themeDetails = useContext(ThemeContext);
+
     return (
         <div className={classNames(css.card , isPending && css.disabledBorderTop)}>
-            {
-                isPending
-                ?
-                <Skeleton style={{width: '260px' , height: '180px'}} borderRadius={8}/>
-                :
-                <img className={css.cardImage} src={house?.image} />
-            }
-            <div className={classNames(css.cardBody , isPending && css.disabledCursor)}>
+            <SkeletonTheme baseColor={themeDetails?.isDark ? '#1e293b' : '#e2e8f0'} highlightColor={themeDetails?.isDark ? "#334155" : '#cbd5e1'}>
                 {
                     isPending
                     ?
-                    <Skeleton style={{width: '50px' , height: '20px'}} borderRadius={8}/>
+                    <Skeleton style={{width: '260px' , height: '180px'}} borderRadius={8} className={css.skeleton} />
                     :
-                    <section className={css.detailsWrapper}>
-                        <FaStar className={css.rateIcon} />
-                        <p className={css.rate}>{house?.rate}</p>
-                    </section>
+                    <img className={css.cardImage} src={house?.image} />
                 }
-                {
-                    isPending
-                    ?
-                    <Skeleton style={{width: '120px' , height: '20px'}} borderRadius={8}/>
-                    :
-                    <section className={css.detailsWrapper}>
-                        <IoPeopleSharp className={css.capacityIcon} />
-                        <p className={css.capacity}>
-                            ظرفیت {house?.details?.capacity} نفر
-                        </p>
-                    </section>
-                }
-                {
-                    isPending
-                    ?
-                    <Skeleton style={{width: '80px' , height: '12px'}} borderRadius={8}/>
-                    :
-                    <section className={css.detailsWrapper}>
-                        <HiOutlineLocationMarker className={css.locationIcon} />
-                        <p className={css.location}>
-                            استان {house?.state}، {house?.city}
-                        </p>
-                    </section>
-                }
-                {!!house?.discount && !isPending && (
-                    <section className={css.detailsWrapper}>
-                        <p className={css.oldPrice}>{house?.price}</p>
-                        <span className={css.discount}>{house?.discount}%</span>
-                    </section>
-                )}
-                {
-                    isPending
-                    ?
-                    <Skeleton style={{width: '150px' , height: '18px'}} borderRadius={8}/>
-                    :
-                    <section className={css.detailsWrapper}>
-                        <p className={css.price}>
-                            {
-                                ((house?.discount !== 0 && !house?.discount) || !house?.price)
-                                ?
-                                null
-                                :
-                                englishToPersian(
-                                    calculateDiscount(house?.discount, house?.price)
-                                )
-                            }
-                            {" "}
-                            تومان
-                        </p>
-                        <p className={css.oneNight}>/ هرشب</p>
-                    </section>
-                }
-            </div>
-            {
-                !isPending &&
-                <div
-                    className={css.likePost}
-                    onClick={() => setIsBeloved((prevState) => !prevState)}
-                >
-                    {isBeloved ? (
-                        <GoHeart />
-                    ) : (
-                        <GoHeartFill className={css.filledHeartIcon} />
+                <div className={classNames(css.cardBody , isPending && css.disabledCursor)}>
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '50px' , height: '20px'}} borderRadius={8}/>
+                        :
+                        <section className={css.detailsWrapper}>
+                            <FaStar className={css.rateIcon} />
+                            <p className={css.rate}>{house?.rate}</p>
+                        </section>
+                    }
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '120px' , height: '20px'}} borderRadius={8}/>
+                        :
+                        <section className={css.detailsWrapper}>
+                            <IoPeopleSharp className={css.capacityIcon} />
+                            <p className={css.capacity}>
+                                ظرفیت {house?.details?.capacity} نفر
+                            </p>
+                        </section>
+                    }
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '80px' , height: '12px'}} borderRadius={8}/>
+                        :
+                        <section className={css.detailsWrapper}>
+                            <HiOutlineLocationMarker className={css.locationIcon} />
+                            <p className={css.location}>
+                                استان {house?.state}، {house?.city}
+                            </p>
+                        </section>
+                    }
+                    {!!house?.discount && !isPending && (
+                        <section className={css.detailsWrapper}>
+                            <p className={css.oldPrice}>{house?.price}</p>
+                            <span className={css.discount}>{house?.discount}%</span>
+                        </section>
                     )}
+                    {
+                        isPending
+                        ?
+                        <Skeleton style={{width: '150px' , height: '18px'}} borderRadius={8}/>
+                        :
+                        <section className={css.detailsWrapper}>
+                            <p className={css.price}>
+                                {
+                                    ((house?.discount !== 0 && !house?.discount) || !house?.price)
+                                    ?
+                                    null
+                                    :
+                                    englishToPersian(
+                                        calculateDiscount(house?.discount, house?.price)
+                                    )
+                                }
+                                {" "}
+                                تومان
+                            </p>
+                            <p className={css.oneNight}>/ هرشب</p>
+                        </section>
+                    }
                 </div>
-            }
+                {
+                    !isPending &&
+                    <div
+                        className={css.likePost}
+                        onClick={() => setIsBeloved((prevState) => !prevState)}
+                    >
+                        {isBeloved ? (
+                            <GoHeart />
+                        ) : (
+                            <GoHeartFill className={css.filledHeartIcon} />
+                        )}
+                    </div>
+                }
+            </SkeletonTheme>
         </div>
     );
 };
